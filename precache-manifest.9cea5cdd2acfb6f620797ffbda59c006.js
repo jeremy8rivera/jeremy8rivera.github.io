739ec9eb83dfe782b46614b9e4a4e9caacfd9947
jeremy8rivera <jeremy8rivera@gmail.com>
2019-09-16 18:32:58 -0400

    Updates

---
+++ precache-manifest.9cea5cdd2acfb6f620797ffbda59c006.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cd2ce0a5563e388ea646cb125f389d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3db3f58380eec474f26",
+    "url": "/static/css/main.b9b01f7c.chunk.css"
+  },
+  {
+    "revision": "628f418d4372d6e1874c",
+    "url": "/static/js/2.c49cda5f.chunk.js"
+  },
+  {
+    "revision": "e3db3f58380eec474f26",
+    "url": "/static/js/main.8ef959e4.chunk.js"
+  },
+  {
+    "revision": "da7e19da5b2d0ab63102",
+    "url": "/static/js/runtime~main.4fedb128.js"
+  },
+  {
+    "revision": "6be58bf74cc051a02f8008d2598d925a",
+    "url": "/static/media/Resume.6be58bf7.pdf"
+  },
+  {
+    "revision": "1118869ee65ca0a46846624fbeb2a9c1",
+    "url": "/static/media/VFSPortfolio.1118869e.pdf"
+  },
+  {
+    "revision": "8601f3be978767dcbb75732154fcc05b",
+    "url": "/static/media/headshot.8601f3be.jpg"
+  }
+]);
