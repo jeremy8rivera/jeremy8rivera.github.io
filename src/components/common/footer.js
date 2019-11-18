c2a81057823b14c3232bc1065c1e038e3c0e2d80
jeremy8rivera <jeremy8rivera@gmail.com>
2019-11-18 17:28:26 -0500

    remove details

--- src/components/common/footer.js
+++ src/components/common/footer.js
@@ -22,7 +22,6 @@ function Footer() {
     		<li>Jeremy Rivera</li>
     		<li>Greenwich, CT</li>
     		<li><a href="mailto: jeremy8rivera@gmail.com">jeremy8rivera@gmail.com</a></li>
-    		<li><a href="tel:+1203-252-9970">+1-(203)-252-9970</a></li>
     	</ul>
     </div>
     </div>
